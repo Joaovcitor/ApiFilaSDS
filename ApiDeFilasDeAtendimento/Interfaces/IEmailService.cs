b14f33205319a9900134de2cd790f631aa7d30e5
joao victor <neguchos96@gmail.com>
2026-01-19 10:20:10 -0300

    adicionado o esqueleto de email

---
+++ ApiDeFilasDeAtendimento/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiDeFilasDeAtendimento.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendWelcomeAsync(string nomeCompleto, string email);
+        Task ResetPasswordAsync(string email, string resetLink);
+    }
+}
---
+++ ApiDeFilasDeAtendimento/Services/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using ApiDeFilasDeAtendimento.Interfaces;
+
+namespace ApiDeFilasDeAtendimento.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task ResetPasswordAsync(string email, string resetLink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendWelcomeAsync(string nomeCompleto, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
